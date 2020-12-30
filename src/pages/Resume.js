ae66cc978b1c03f590f4164ec350075ac172f789
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-12-30 00:44:22 -0500

    working setup

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Jumbotron from "../components/Jumbotron";
+import SaveIcon from "@material-ui/icons/Save";
 
 function Resume() {
-  return <div> Resume Page</div>;
+  return (
+    <Jumbotron>
+      <div className="row" id="contact-resume-row">
+        <div
+          className="btn-group m-auto"
+          role="group"
+          aria-label="Contact Buttons"
+        >
+          <Link
+            to="assets/pdf/WilmarAStephens.pdf"
+            className="btn btn-secondary btn-lg active"
+            download="WilmarStephens.pdf"
+          >
+            <SaveIcon />
+            Download Resume
+          </Link>
+        </div>
+      </div>
+    </Jumbotron>
+  );
 }
 
 export default Resume;
